063b8bc754845c7edcdcb99a1aa3cac234a3c813
kevin1688 <jackexi@gmail.com>
2022-10-18 22:22:58 +0800

    Initial Commit

---
+++ kkbox/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  kkbox
+//
+//  Created by kai wen chen on 2022/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kkbox/kkboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  kkboxApp.swift
+//  kkbox
+//
+//  Created by kai wen chen on 2022/10/18.
+//
+
+import SwiftUI
+
+@main
+struct kkboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
