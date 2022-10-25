3c6dc3f1b2295f39ee30dabd4488b8b614038382
kevin1688 <jackexi@gmail.com>
2022-10-25 21:47:55 +0800

    add api

---
+++ kkbox/API.swift
@@ -0,0 +1,15 @@
+//
+//  API.swift
+//  kkbox
+//
+//  Created by kai wen chen on 2022/10/25.
+//
+
+import Foundation
+
+class API {
+      static let apiKey = "5ca29d33e82e0f12e4aafdf83e0a2077"
+      static let weather = "weather"
+      static let forecast = "forecast"
+      static let imperial = "imperial"
+}
--- kkbox/ContentView.swift
+++ kkbox/ContentView.swift
@@ -8,15 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var joke: String = ""
+    
+    //abcd
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(joke)
+            Button {
+                Task {
+                                let (data, _) = try await URLSession.shared.data(from: URL(string:"https://api.chucknorris.io/jokes/random")!)
+                                let decodedResponse = try? JSONDecoder().decode(Joke.self, from: data)
+                                joke = decodedResponse?.value ?? ""
+                            }
+            } label: {
+                Text("擷取資料")
+            }
+
         }
         .padding()
     }
+}
+
+struct Joke: Codable {
+    let value: String
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -24,3 +43,31 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+func fetchForecastDataFromCity(city: String) -> String {
+        var des = ""
+        let urlstr = "https://api.openweathermap.org/data/2.5/\(API.forecast)?q=\(city)&appid=\(API.apiKey)&units=\(API.imperial)&lang=zh_tw"
+        print(urlstr)
+        if let url = URL(string: urlstr) {
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .secondsSince1970
+                if let data = data {
+                    do {
+                        des = data.description
+                        /*
+                        let searchResponse = try decoder.decode(ForecastWeather.self, from: data)
+                        self.forecastInfo = searchResponse
+                        DispatchQueue.main.async {
+                            self.forecastRow = (self.forecastInfo?.list)!
+                            self.tableView.reloadData()
+                        }*/
+                    } catch {
+                        des = "error"
+                        print("error")
+                    }
+                }
+            }.resume()
+        }
+    return des
+    }
